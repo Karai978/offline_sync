1449cbc1bd358794b0e66113cfd893130f12b01d
valimbavaka-hub <valimbavakarajonasy.com>
2026-09-11 08:56:47 +0300

    Versionning

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     "version": "17.0.1.0.0",
     "summary": "Synchronisation des données saisies hors ligne (PWA) vers Odoo",
     "category": "Technical",
-    "author": "VLR.33",
+    "author": "VLR.34",
     "depends": ["base", "web"],
     "data": [
         "security/ir.model.access.csv",
